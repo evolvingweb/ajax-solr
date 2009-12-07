e421e54bd35b4ec6e0c21b0f5766a20145537820
James McKinney <james@slashpoundbang.com>
2009-12-07 16:38:55 -0500

    add index.html for each step of tutorial

--- examples/reuters/CurrentSearchWidget.js
+++ examples/reuters/CurrentSearchWidget.js
@@ -1,53 +1,45 @@
 (function ($) {
 
 AjaxSolr.CurrentSearchWidget = AjaxSolr.AbstractWidget.extend({
-  displayQuery: function (queryObj) {//TODO
-    var count = this.countLinks(queryObj);
-    if (count) {
-      AjaxSolr.theme('list_items', this.target, this.unclickLinks(queryObj, count));
-    }
-    else {
-      $(this.target).html('<div>Viewing all documents!</div>');
-    }
-  },
-
-  countLinks: function (queryObj) {//TODO
-    var count = 0;
-    if (queryObj.q) {
-      count++;
-    }
-    else {
-      for (var i in queryObj.fq) {
-        count++;
-      }
-    }
-    return count;
-  },
-
-  unclickLinks: function (queryObj, count) {//TODO
+  afterRequest: function () {
+    var self = this;
     var links = [];
 
-    var me = this;
-    if (count > 1) {
-      links.push($('<a href="#"/>').text('remove all').click(function () {
-        me.manager.widgets.text.clear();
-        me.manager.store.remove('fq');
-        me.manager.doRequest(0);
+    var q = this.manager.store.get('q').val();
+    if (q) {
+      links.push($('<a href="#"/>').text('(x) ' + q).click(function () {
+        if (self.manager.store.remove('q')) {
+          self.manager.doRequest(0);
+        }
         return false;
       }));
     }
 
-    if (queryObj.q) {
-      var widget = this.manager.widgets.text;
-      links.push($('<a/>').text('(x) ' + queryObj.q).click(widget.unclickHandler());
+    var fq = this.manager.values('fq');
+    for (var i = 0, l = fq.length; i < l; i++) {
+      links.push($('<a href="#"/>').text('(x) ' + fq[i]).click(function () {
+        if (self.manager.store.removeByValue('fq', fq[i])) {
+          self.manager.doRequest(0);
+        }
+        return false;
+      }));
     }
 
-    for (var i in queryObj.fq) {
-      var widget = this.manager.widgets[queryObj.fq[i].widgetId], facet = queryObj.fq[i].value;
-      links.push($('<a/>').text('(x) ' + decodeURIComponent(queryObj.fq[i].toString())).click(widget.unclickHandler(facet));
+    if (links.length > 1) {
+      links.unshift($('<a href="#"/>').text('remove all').click(function () {
+        self.manager.store.remove('q');
+        self.manager.store.remove('fq');
+        self.manager.doRequest(0);
+        return false;
+      }));
     }
 
-    return links;
+    if (links.length) {
+      AjaxSolr.theme('list_items', this.target, links);
+    }
+    else {
+      $(this.target).html('<div>Viewing all documents!</div>');
+    }
   }
 });
 
