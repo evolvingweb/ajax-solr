8bd788848d491c5f829388c05e3029d13d3de497
James McKinney <james@slashpoundbang.com>
2009-12-09 17:11:15 -0500

    update files for tutorial

---
+++ examples/reuters/widgets/AutocompleteWidget.7.0.js
@@ -0,0 +1,41 @@
+(function ($) {
+
+AjaxSolr.AutocompleteWidget = AjaxSolr.AbstractFacetWidget.extend({
+  afterRequest: function () {
+    $(this.target).find('input').val('');
+
+    var self = this;
+
+    var list = [];
+    for (var i = 0; i < this.fields.length; i++) {
+      var field = this.fields[i];
+      for (var facet in this.manager.response.facet_counts.facet_fields[field]) {
+        list.push({
+          field: field,
+          value: facet,
+          text: facet + ' (' + this.manager.response.facet_counts.facet_fields[field][facet] + ') - ' + field
+        });
+      }
+    }
+
+    this.requestSent = false;
+    $(this.target).find('input').autocomplete(list, {
+      formatItem: function(facet) {
+        return facet.text;
+      }
+    }).result(function(e, facet) {
+      self.requestSent = true;
+      if (self.manager.store.addByValue('fq', facet.field + ':' + facet.value)) {
+        self.manager.doRequest(0);
+      }
+    }).bind('keydown', function(e) {
+      if (self.requestSent === false && e.which == 13) {
+        if (self.add($(this).val())) {
+          self.manager.doRequest(0);
+        }
+      }
+    });
+  }
+});
+
+})(jQuery);
--- examples/reuters/widgets/AutocompleteWidget.js
+++ examples/reuters/widgets/AutocompleteWidget.js
@@ -4,38 +4,45 @@ AjaxSolr.AutocompleteWidget = AjaxSolr.AbstractFacetWidget.extend({
   afterRequest: function () {
     $(this.target).find('input').val('');
 
-    // TODO need to get facet values without limit
-
-    var list = [];
-    for (var i = 0; i < this.fields.length; i++) {
-      var field = this.fields[i];
-      for (var facet in this.manager.response.facet_counts.facet_fields[field]) {
-        list.push({
-          field: field,
-          value: facet,
-          text: facet + ' (' + this.manager.response.facet_counts.facet_fields[field][facet] + ') - ' + field
-        });
-      }
-    }
-
     var self = this;
-    this.fromAutocomplete = false;
-    $(this.target).find('input').autocomplete(list, {
-      formatItem: function(facet) {
-        return facet.text;
-      }
-    }).result(function(e, facet) {
-      self.fromAutocomplete = true;
-      if (self.manager.store.addByValue('fq', facet.field + ':' + facet.value)) {
-        self.manager.doRequest(0);
-      }
-    }).bind('keydown', function(e) {
-      if (self.fromAutocomplete === false && e.which == 13) {
-        if (self.add($(this).val())) {
-          self.manager.doRequest(0);
+
+    var callback = function (response) {
+      var list = [];
+      for (var i = 0; i < self.fields.length; i++) {
+        var field = self.fields[i];
+        for (var facet in response.facet_counts.facet_fields[field]) {
+          list.push({
+            field: field,
+            value: facet,
+            text: facet + ' (' + response.facet_counts.facet_fields[field][facet] + ') - ' + field
+          });
         }
       }
-    });
+
+      self.requestSent = false;
+      $(self.target).find('input').autocomplete(list, {
+        formatItem: function(facet) {
+          return facet.text;
+        }
+      }).result(function(e, facet) {
+        self.requestSent = true;
+        if (self.manager.store.addByValue('fq', facet.field + ':' + facet.value)) {
+          self.manager.doRequest(0);
+        }
+      }).bind('keydown', function(e) {
+        if (self.requestSent === false && e.which == 13) {
+          if (self.add($(self).val())) {
+            self.manager.doRequest(0);
+          }
+        }
+      });
+    } // end callback
+
+    var params = [ 'q=*:*&facet=true&facet.limit=-1&facet.mincount=1&json.nl=map' ];
+    for (var i = 0; i < this.fields.length; i++) {
+      params.push('facet.field=' + this.fields[i]);
+    }
+    jQuery.getJSON(this.manager.solrUrl + '?' + params.join('&') + '&wt=json&json.wrf=?', {}, callback);
   }
 });
 
