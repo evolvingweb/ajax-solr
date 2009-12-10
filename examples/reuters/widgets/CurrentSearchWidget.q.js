28d92ceb8feb408258a1bc3b2c71faf8f240b560
James McKinney <james@slashpoundbang.com>
2009-12-10 13:44:43 -0500

    add files in which autocomplete and currentsearch are aware of the q parameter

---
+++ examples/reuters/widgets/CurrentSearchWidget.q.js
@@ -0,0 +1,50 @@
+(function ($) {
+
+AjaxSolr.CurrentSearchWidget = AjaxSolr.AbstractWidget.extend({
+  afterRequest: function () {
+    var self = this;
+    var links = [];
+
+    var q = this.manager.store.get('q').val();
+    if (q != '*:*') {
+      links.push($('<a href="#"/>').text('(x) ' + q).click(function () {
+        self.manager.store.get('q').val('*:*');
+        self.manager.doRequest(0);
+        return false;
+      }));
+    }
+
+    var fq = this.manager.store.values('fq');
+    for (var i = 0, l = fq.length; i < l; i++) {
+      links.push($('<a href="#"/>').text('(x) ' + fq[i]).click(self.removeFacet(fq[i])));
+    }
+
+    if (links.length > 1) {
+      links.unshift($('<a href="#"/>').text('remove all').click(function () {
+        self.manager.store.get('q').val('*:*');
+        self.manager.store.remove('fq');
+        self.manager.doRequest(0);
+        return false;
+      }));
+    }
+
+    if (links.length) {
+      AjaxSolr.theme('list_items', this.target, links);
+    }
+    else {
+      $(this.target).html('<div>Viewing all documents!</div>');
+    }
+  },
+
+  removeFacet: function (facet) {
+    var self = this;
+    return function () {
+      if (self.manager.store.removeByValue('fq', facet)) {
+        self.manager.doRequest(0);
+      }
+      return false;
+    };
+  }
+});
+
+})(jQuery);
