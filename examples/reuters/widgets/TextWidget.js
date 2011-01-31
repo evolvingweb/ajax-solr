bfaa43bf4a4e3753a7eddf95672e90a3cc30d803
James McKinney <james@slashpoundbang.com>
2011-01-31 17:03:50 -0500

    asankha: move bind to init

--- examples/reuters/widgets/TextWidget.js
+++ examples/reuters/widgets/TextWidget.js
@@ -1,9 +1,7 @@
 (function ($) {
 
 AjaxSolr.TextWidget = AjaxSolr.AbstractFacetWidget.extend({
-  afterRequest: function () {
-    $(this.target).find('input').val('');
-
+  init: function () {
     var self = this;
     $(this.target).find('input').bind('keydown', function(e) {
       if (e.which == 13) {
@@ -13,6 +11,10 @@ AjaxSolr.TextWidget = AjaxSolr.AbstractFacetWidget.extend({
         }
       }
     });
+  },
+
+  afterRequest: function () {
+    $(this.target).find('input').val('');
   }
 });
 
