43f873b41baf46034129bfd742bd689ffeb17a15
James McKinney <james@slashpoundbang.com>
2011-07-19 10:53:18 -0400

    do not return rows in AutocompleteWidget

--- examples/reuters/widgets/AutocompleteWidget.js
+++ examples/reuters/widgets/AutocompleteWidget.js
@@ -47,7 +47,7 @@ AjaxSolr.AutocompleteWidget = AjaxSolr.AbstractFacetWidget.extend({
       });
     } // end callback
 
-    var params = [ 'q=*:*&facet=true&facet.limit=-1&facet.mincount=1&json.nl=map' ];
+    var params = [ 'q=*:*&rows=0&facet=true&facet.limit=-1&facet.mincount=1&json.nl=map' ];
     for (var i = 0; i < this.fields.length; i++) {
       params.push('facet.field=' + this.fields[i]);
     }
