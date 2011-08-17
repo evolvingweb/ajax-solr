af5f66ca27ec179c43a0fea36dfdddc03a02f8dc
David Smiley <david.w.smiley@gmail.com>
2011-08-17 00:41:31 -0400

    Improved comments, made tokenization and lowercasing and the autocomplete minLength public uptions.

--- widgets/jquery/AutocompleteTermWidget.js
+++ widgets/jquery/AutocompleteTermWidget.js
@@ -7,18 +7,24 @@
  * implementation uses Solr's facet.prefix technique. This technique benefits
  * from honoring the filter query state and by being able to put words prior to
  * the last one the user is typing into a filter query as well to get even more
- * relevant completion suggestions. Remember to put a facet warming query into 
- * Solr's "firstSearcher" in solrconfig.xml, for the target field.  Remember to
- * use appropriate text analysis to include a tokenizer (not keyword) and do 
+ * relevant completion suggestions.
+ *
+ * Index instructions: 
+ * Put a facet warming query into Solr's "firstSearcher" in solrconfig.xml, for 
+ * the target field.
+ * Use appropriate text analysis to include a tokenizer (not keyword) and do 
  * <i>not</i> do stemming or else you will see stems suggested. A 'light' 
  * stemmer may produce acceptable stems.
+ * If you are auto-completing in a search box that would normally be using
+ * the dismax query parser AND your qf parameter references more than one field,
+ * then you might want to use a catch-all search field to autocomplete on.
  *
  * For large indexes, another implementation approach like 
  * the Suggester feature or TermsComponent might be better than a faceting
  * approach. 
  *
- * FYI: Other types of autocomplete are search-results, query-log, and 
- * facet-value. Again, this one is term-completion.
+ * FYI: Other types of autocomplete (AKA suggest) are search-results, query-log, 
+ * and facet-value. Again, this one is term autocompletion.
  *
  * @author David Smiley -- david.w.smiley at gmail.com
  */
@@ -41,6 +47,30 @@ TODO:
   field: null,
   
   /**
+   * Wether the underlying field is tokenized. This component will take words
+   * before the last word (whitespace separated) and generate a filter query
+   * for those words, while only the last word will be used for facet.prefix.
+   * For field-value completion (on just one field) or query log completion, 
+   * you would have a non-tokenized field to complete against.
+   * 
+   * @field
+   * @public
+   * @type Boolean
+   * @default true
+   */ 
+  tokenized: true,
+  
+  /**
+   * Indicates wether to lowercase the facet.prefix value.
+   *
+   * @field
+   * @public
+   * @type Boolean
+   * @default true
+   */
+  lowercase: true,
+  
+  /**
    * The maximum number of results to show.
    *
    * @field
@@ -49,6 +79,16 @@ TODO:
    * @default 10
    */
   limit: 10,
+  
+  /**
+   * The minimum number of characters required to show suggestions.
+   *
+   * @field
+   * @public
+   * @type Number
+   * @default 2
+   */
+  minLength: 2,
   
   /**
    * The URL path that follows the solr webapp, for use in auto-complete
@@ -85,18 +125,22 @@ TODO:
       source: function( request, response ) {
         
         var params = {};
-        //-- take the query string and split out the last word from the words
-        // before it.
+
         var qInput = $(self.target).find('input').val().trim();
-        var qFilter = "";//before the last word
-        var qPrefix = qInput;//the last word
-        var lastSpace = qInput.lastIndexOf(' ');
-        if (lastSpace > -1) {
-          qFilter = qInput.substring(0,lastSpace);
-          qPrefix = qInput.substring(lastSpace+1);
-          params['fq'] = self.field + ':' + AjaxSolr.Parameter.escapeValue(qFilter);//this phrases it,
+        var qPrefix = qInput;//facet.prefix value
+        var qFilter = "";//before the last word (if we tokenize)
+        if (self.tokenized) {
+          //-- take the query string and split out the last word from the words
+          // before it.
+          var lastSpace = qInput.lastIndexOf(' ');
+          if (lastSpace > -1) {
+            qFilter = qInput.substring(0,lastSpace);
+            qPrefix = qInput.substring(lastSpace+1);
+            params['fq'] = '{!dismax qf='+self.field+'}'+qFilter;
+          }
         }
-        qPrefix = qPrefix.toLowerCase();
+        if (self.lowercase)
+          qPrefix = qPrefix.toLowerCase();
         
         //get filter queries in effect now
         var fqsUrl = '';
@@ -126,7 +170,7 @@ TODO:
           }
         });
       },
-      minLength: 2,
+      minLength: self.minLength,
       select: function( event, ui ) {
         if (self.set(ui.item.value)) {
           self.manager.doRequest(0);
