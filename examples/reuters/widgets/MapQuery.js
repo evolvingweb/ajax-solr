3a8fb2895927e3146c9a0116826400c88e0f4fb4
Gabriel <gab.ceron@gmail.com>
2013-12-20 02:54:35 +0100

    Code refactored and cleaned

--- examples/reuters/widgets/MapQuery.js
+++ examples/reuters/widgets/MapQuery.js
@@ -1,19 +1,19 @@
 (function ($) {
 var ini = false;
-var markers;
 
 AjaxSolr.MapQuery = AjaxSolr.AbstractWidget.extend({
-	start: 0,
 	currentGeolocQuery: null,
+	currentMarkers: null,
 	gridSize: 5,
+	zoomThreshold: 5,
 	
 	init: function () {
 		self= this;
 		$(this.target).html($('<img>').attr('src', 'images/ajax-loader.gif'));
+		//Update button
 		$("#button").append("<button id='updatebutton' >Update </button>");
-		//Hook for update button
 		$("#button").click( function(){self._updateFunction()});
-		//crear mapar
+		//Leaflet map
 		var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/BC9A493B41014CAABB98F0471D759707/997/256/{z}/{x}/{y}.png',
 			cloudmadeAttribution = 'Map data &copy; 2011 OpenStreetMap contributors, Imagery &copy; 2011 CloudMade, Points &copy 2012 LINZ',
 			cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 17, attribution: cloudmadeAttribution}),
@@ -26,14 +26,10 @@ AjaxSolr.MapQuery = AjaxSolr.AbstractWidget.extend({
 	
 	beforeRequest: function () {
 	    //Remove current layer of markers if we find
-	    if ( markers )
+	    if ( self.currentMarkers )
 	    {
-		map.removeLayer(markers);
+			map.removeLayer(self.currentMarkers);
 	    }
-	//////////////
-
-	////////////
-
 	},
 
 	facetLinks: function (facet_field, facet_values) {return false;},
@@ -43,48 +39,33 @@ AjaxSolr.MapQuery = AjaxSolr.AbstractWidget.extend({
 	buttonRequest: function prova(){   alert("hpl");  },
 
 	afterRequest: function () {
-		self.clusterFunction(); //funcion de Tomás
-		//es torna a omplir el mapa
-		//TODO: cargar las coordenadas de los comentarios
-		//
-		//console.log("hola");
-		//markers = new L.MarkerClusterGroup();
-		/*for( var i=0; i<this.manager.response.response.docs.length; i++)
-		{
-			var title = this.manager.response.response.docs[i].comment_content;
-			var a =this.manager.response.response.docs[i].geo_loc[0].split(",");
-			var marker = new L.Marker(new L.LatLng(parseFloat(a[0]), parseFloat(a[1])), { title: title });
-			marker.bindPopup(title);
-			markers.addLayer(marker);
-		}
-		
-		map.addLayer(markers);
-   		Manager.store.addByValue('boundingBox', map.getBounds() );*/
-		//$(".leaflet-top.leaflet-right").append("<button id='update_button'> Update </button>");
+		self._gridPivot(); 
 	},
 	
-	clusterFunction: function(){ //pseudocodigo tomás
-		//TODO coger lso bounds de la propia respueta porque puede ser que se haya modificado el mapa (de momento dejamos el mapa quieto)
+	template: function (doc) {return false;},
+	
+	//Function that put the markers on the map
+	_gridPivot: function(){ 
+		//Map bounds
 		var coordenadas= map.getBounds();
 		var coordenadas_xmin=coordenadas._southWest.lat;
 		var coordenadas_xmax=coordenadas._northEast.lat;
 		var coordenadas_ymin=coordenadas._southWest.lng;
 		var coordenadas_ymax=coordenadas._northEast.lng;
-		var objetoJsonIdentifier= "";
-		objetoJsonIdentifier= map.getZoom()<5 ? "_lat_zero,_long_zero": "_lat,_long";
-		//if(map.getZoom()>5){objetoJsonIdentifier="_lat_zero,_long_zero";}else{objetoJsonIdentifier="_lat,_long";}
-		//Tamaño de las celdas
+		var jsonIdentifier= "";
+		jsonIdentifier= map.getZoom()<self.zoomThreshold ? "_lat_zero,_long_zero": "_lat,_long";
+		//Grid size
 		var stepx= Math.abs( (coordenadas_xmin-coordenadas_xmax)/self.gridSize );
 		var stepy= Math.abs( (coordenadas_ymin-coordenadas_ymax)/self.gridSize );
-		//Parseo del json
-		
-		var objetoJson = this.manager.response.facet_counts.facet_pivot[objetoJsonIdentifier];
-		markers = new L.LayerGroup();
+		var objetoJson = this.manager.response.facet_counts.facet_pivot[jsonIdentifier];
+		self.currentMarkers = new L.LayerGroup();
 		var countTotal=0;
+		//Total of pivots 
 		for (var i in objetoJson)
 		{
 			countTotal+=objetoJson[i].count;
 		}
+		//Calculation of pivots for every cell of the grid
 		for(var i= coordenadas_xmin; i<coordenadas_xmax;i+=stepx)
 		{
 		
@@ -110,30 +91,25 @@ AjaxSolr.MapQuery = AjaxSolr.AbstractWidget.extend({
 						
 					}
 				}
-				
+				//If there are any document in this cell...
 				if(currentCelda!=0)
-				{
-					//var marker = new L.Marker(new L.LatLng(parseFloat(i+stepx/2), parseFloat(j+stepy/2)), { title: currentCelda });
-					//markers.addLayer(marker);
-				    
-				    var size;
+				{	    
+				    //Different colors for different weight related to the total
+					var size;
 				    var c = parseFloat(currentCelda)/countTotal;
 				    if(c<1/3)size = "small";
 				    else if(c <2/3) size = "medium";
 				    else size = "large";
-
+					//Insert marker at grid's center
 				    var myIcon = new L.DivIcon({ html: '<div><span>' + currentCelda + '</span></div>', className: 'leaflet-marker-icon marker-cluster marker-cluster-'+size , iconSize: new L.Point(40, 40) });
-				    //markers.addLayer(marker);
 				    m2=new L.Marker(new L.LatLng(parseFloat(i+stepx/2), parseFloat(j+stepy/2)), {icon: myIcon, title: currentCelda});
-				    markers.addLayer(m2);
+				    self.currentMarkers.addLayer(m2);
 				}
 			}
 		}
-		map.addLayer(markers);
+		map.addLayer(self.currentMarkers);
 	},
-
-	template: function (doc) {return false;},
-
+	//Called when we click the updateButton
 	_updateFunction: function(){
 		var geoLoc= map.getBounds();
 		//Remove previous geo_loc filter query
@@ -141,10 +117,10 @@ AjaxSolr.MapQuery = AjaxSolr.AbstractWidget.extend({
 		//Add new filter query and refresh current filter query
 		this.currentGeolocQuery= 'geo_loc:['+geoLoc._southWest.lat+','+geoLoc._southWest.lng+' TO '+geoLoc._northEast.lat+','+geoLoc._northEast.lng+']';
 		Manager.store.addByValue('fq', self.currentGeolocQuery);
+		//Different request for different levels of zoom, round at solr
 		Manager.store.remove('facet.pivot');
-		if(map.getZoom()<5) Manager.store.addByValue('facet.pivot', '_lat_zero,_long_zero');
+		if(map.getZoom()<self.zoomThreshold) Manager.store.addByValue('facet.pivot', '_lat_zero,_long_zero');
 		else Manager.store.addByValue('facet.pivot', '_lat,_long');
-        //Línea añadida para los facets
 		Manager.doRequest();
 	}
   
