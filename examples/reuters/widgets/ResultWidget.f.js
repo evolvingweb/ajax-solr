efe96c092dbcd74f3cf21dcb4c50a8417aaebf6c
James McKinney <james@slashpoundbang.com>
2015-02-23 18:19:15 -0500

    fix facetHandler and pagination

--- examples/reuters/widgets/ResultWidget.f.js
+++ examples/reuters/widgets/ResultWidget.f.js
@@ -32,7 +32,7 @@ AjaxSolr.ResultWidget = AjaxSolr.AbstractWidget.extend({
     return function () {
       self.manager.store.remove('fq');
       self.manager.store.addByValue('fq', facet_field + ':' + AjaxSolr.Parameter.escapeValue(facet_value));
-      self.doRequest();
+      self.doRequest(0);
       return false;
     };
   },
